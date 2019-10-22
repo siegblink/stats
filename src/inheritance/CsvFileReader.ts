0ea7d00433fce9792661e77bde2546a3df4d08b6
Sieg <siegblink@gmail.com>
2019-10-22 18:30:33 +0800

    Add a different implementation of CsvFileReader and MatchReader using composition

--- src/inheritance/CsvFileReader.ts
+++ src/inheritance/CsvFileReader.ts
@@ -1,8 +1,9 @@
 import fs from 'fs'
 
-export class CsvFileReader {
-  data: string[][] = []
+export abstract class CsvFileReader<T> {
+  data: T[] = []
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T
 
   read(): void {
     this.data = fs
@@ -13,5 +14,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',')
       })
+      .map(this.mapRow)
   }
 }
