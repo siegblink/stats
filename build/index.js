4bb43ed9094fcbbc20082715bc38b5caa6bdaad6
Sieg <siegblink@gmail.com>
2019-12-17 17:23:56 +0800

    Update build directory

--- build/index.js
+++ build/index.js
@@ -2,21 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var MatchResult_1 = require("./MatchResult");
 // create an object that satisfies the 'DataReader' interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // create an instance of MatchReader and pass in something
 // that satisfies the 'DataReader' interface
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log("Man United won " + manUnitedWins + " games");
+// console.log(`Man United won ${manUnitedWins} games`)
